8b382072e77bd7807861df638006bcda49f8b58d
Mert As <meras@uni-bremen.de>
2024-08-05 23:21:42 +0200

    Fix harvester

--- ckanext/myharvester/plugin.py
+++ ckanext/myharvester/plugin.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
 
 
